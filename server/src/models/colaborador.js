cd6197fca78103a0e6c8be5fbf722cfa23a6f642
Bruno dos Santos R. de Siqueira <bsiqueira@geogas.com.br>
2022-03-07 19:04:13 -0300

    Api de pagamento funcionando

--- server/src/models/colaborador.js
+++ server/src/models/colaborador.js
@@ -1,89 +1,88 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const colaborador = new Schema({
-    nome: {
-        type: String,
-        required: [true, 'Nome é obrigatório']
-    }, 
-    foto: String,
-    email: {
-        type: String,
-        required: [true, 'E-mail é obrigatório.']
+  nome: {
+    type: String,
+    required: [true, "Nome é obrigatório"],
+  },
+  foto: String,
+  email: {
+    type: String,
+    required: [true, "E-mail é obrigatório."],
+  },
+  senha: {
+    type: String,
+    default: null,
+  },
+  telefone: String,
+  // endereco: {
+  //     cidade: String,
+  //     uf: String,
+  //     cep: String,
+  //     numero: String,
+  //     pais: String
+  // },
+  dataNascimento: {
+    type: String, // YYYY-MM-DD
+    required: true,
+  },
+  sexo: {
+    type: String,
+    enum: ["M", "F"],
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ["A", "I"],
+    default: "A",
+  },
+  pessoa: {
+    type: String,
+    enum: ["individual", "company"],
+    required: true,
+    default: "F",
+  },
+  contaBancaria: {
+    titular: {
+      type: String,
+      required: true,
     },
-    senha: {
-        type: String,
-        default: null
+    cpfCnpj: {
+      type: String,
+      required: true,
     },
-    telefone: String,
-    // endereco: {
-    //     cidade: String,
-    //     uf: String,
-    //     cep: String,
-    //     numero: String,
-    //     pais: String
-    // },
-    dataNascimento: {
-        type: String, // YYYY-MM-DD
-        required: true,
+    banco: {
+      type: String,
+      required: true,
     },
-    sexo: {
-        type: String,
-        enum: ['M', 'F'],
-        required: true
+    tipo: {
+      type: String,
+      enum: ["checking", "savings"],
+      required: true,
     },
-    status: {
-        type: String,
-        required: true,
-        enum: ['A', 'I'],
-        default: 'A'
+    agencia: {
+      type: String,
+      required: true,
     },
-    pessoa: {
-        type: String,
-        enum: ['F', 'J'],
-        required: true,
-        default: 'F'
+    numero: {
+      type: String,
+      required: true,
     },
-    contaBancaria: {
-        titular: {
-            type: String,
-            required: true
-        },
-        cpfCnpj: {
-            type: String,
-            required: true
-        },
-        banco: {
-            type: String,
-            required: true
-        },
-        tipo: {
-            type: String,
-            enum: ['checking', 'savings'],
-            required: true
-        },
-        agencia: {
-            type: String,
-            required: true
-        },
-        numero: {
-            type: String,
-            required: true
-        },
-        dv: {
-            type: String,
-            required: true
-        }
+    dv: {
+      type: String,
+      required: true,
     },
-    recipientId: {
-        type: String,
-        required: true
-    },
-    dataCadastro: {
-        type: Date,
-        default: Date.now
-    }
-
+  },
+  recipientId: {
+    type: String,
+    // required: true,
+  },
+  dataCadastro: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('Colaborador', colaborador)
+module.exports = mongoose.model("Colaborador", colaborador);
--- server/src/services/pagarme.js
+++ server/src/services/pagarme.js
@@ -1,22 +1,24 @@
-const api_key = require('../data/keys.json').api_key;
-const axios = require('axios')
+const axios = require("axios");
 
 const api = axios.create({
-    baseURL: 'https://api.pagar.me/core/v5/',
-    headers: {               
-        'Authorization': 'Basic' + Buffer.from(`${api_key}`).toString('base64'),
-        'Content-Type': 'application/json'              
-    },
-})
+  baseURL: "https://api.pagar.me/core/v5/",
+  headers: {
+    Authorization:
+      "Basic " + Buffer.from(`sk_test_MY6NAGzsWH6R01yD:`).toString("base64"),
+    "Content-Type": "application/json",
+  },
+});
 
-// module.exports = async (endpoint, data) => {
-//     try{
-//         const response = await api.post(endpoint, ...data)
-//         return {error: false, data: response.data}
-//     }catch(err){
-//         return {
-//             error: true,
-//             message: JSON.stringify(err.response.data.errors[0])
-//         }
-//     }
-// }
+module.exports = async (endpoint, data) => {
+  try {
+    const response = await api.post(endpoint, data);
+
+    return { error: false, data: response };
+  } catch (err) {
+    return {
+      error: true,
+      message: err.message,
+      debug: JSON.stringify(err.response.data.errors[0]),
+    };
+  }
+};
